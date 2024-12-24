ca3678dc7325e8a6f3ec791669203d4e773b7037
Noah Sundberg <noahsundberg@gmail.com>
2024-12-24 08:56:55 -0600

    Padding on home page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -68,7 +68,7 @@ export default function Index({ loaderData }: Route.ComponentProps) {
   };
 
   return (
-    <div className="flex h-screen items-center justify-center">
+    <div className="p-6 flex h-screen items-center justify-center">
       <div className="flex flex-col items-center gap-16">
         <header className="flex flex-col items-center gap-9">
           <h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
